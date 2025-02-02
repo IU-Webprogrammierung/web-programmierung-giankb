15a7c487ca6e3d0b9097039582a22eded936bc5c
DESKTOP-120F7OB\Win <gianlucabattisti96@gmail.com>
2025-02-02 16:04:57 +0100

    feat(home): home component update (hero, box key sections)

---
+++ web-programmierung/src/components/Box.jsx
@@ -0,0 +1,13 @@
+import "./Box.css"
+export default function Box(){
+    return (
+        <div className="box-container">
+            <img className="box-image" src="/at-at.webp" alt="AT AT image" width="1280" height="800"/>
+            <p className="box-text">
+                Star Wars is more than just a movie saga. It's a cultural phenomenon that has inspired generations since 1977.
+                From the legendary battle between Jedi and Sith to the breathtaking planets and advanced technology, Star Wars is one of the greatest sci-fi stories ever told.
+                Created by George Lucas, it´s a sag about hope, destiny, and the battle between good and evil.
+            </p>
+        </div>
+    )
+}
--- web-programmierung/src/pages/Home.jsx
+++ web-programmierung/src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import Navigation from "../ui/Navigation.jsx";
+import HeroSection from "../ui/HeroSection.jsx";
+import "./Home.css"
+import Box from "../components/Box.jsx";
+import KeySection from "../components/KeySection.jsx";
 
 export default function Home () {
     return (
         <>
             <Navigation></Navigation>
-            <p>Home</p>
+            <HeroSection></HeroSection>
+            <main className="main">
+                <h1 className="home-title">What is star wars</h1>
+                <Box></Box>
+                <KeySection></KeySection>
+            </main>
         </>
     )
 }
