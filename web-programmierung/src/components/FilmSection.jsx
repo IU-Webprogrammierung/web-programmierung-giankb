e999ca519196654a5eb47e0696756dd3d9e6a486
DESKTOP-120F7OB\Win <gianlucabattisti96@gmail.com>
2025-02-04 16:50:24 +0100

    feat(saga): new film section

---
+++ web-programmierung/src/components/FilmSection.jsx
@@ -0,0 +1,28 @@
+import "./FilmSection.css"
+export default function FilmSection({film}){
+    console.log(film)
+    return (
+        <section className="film-section">
+            {film.position ? (
+                <>
+                    <img src={film.imgUrl} alt="Movie poster" className="film-poster" height="" width=""/>
+                    <div className="film-description">
+                        <h2>{film.title}</h2>
+                        <p className="film-description-p">{film.description}</p>
+                        <a className="link" href={film.link} target="blank" rel="noopener noreferrer">Details <small>(starwars.com)</small>
+                            </a>
+                    </div>
+                </>
+            ) : (
+                <>
+                <div className="film-description">
+                        <h2>{film.title}</h2>
+                        <p className="film-description-p">{film.description}</p>
+                        <a className="link" href={film.link} target="blank" rel="noopener noreferrer">Details <small>(starwars.com)</small></a>
+                    </div>
+                    <img src={film.imgUrl} alt="Movie poster" className="film-poster" height="" width=""/>
+                </>
+            )}
+        </section>
+    )
+}
--- web-programmierung/src/pages/Saga.jsx
+++ web-programmierung/src/pages/Saga.jsx
@@ -1,13 +1,30 @@
 import Navigation from "../ui/Navigation.jsx";
 import HeroSection from "../components/HeroSection.jsx";
 import Footer from "../ui/Footer.jsx";
+import FilmSection from "../components/FilmSection.jsx";
+import Carousel from "../components/Caousel.jsx";
+import "./Saga.css"
+import {spinOff, starWarsFilms} from "../services/CONSTANTS.js";
+import ScrollToTop from "../ui/ScrollTop.jsx";
 
 export default function Saga () {
+
     return (
         <>
+            <ScrollToTop />
             <Navigation></Navigation>
-            <HeroSection title={"The Saga"} small={"Canon and not"}></HeroSection>
-            <p>Saga</p>
+            <HeroSection title={"The Saga"} small={"Main films"}></HeroSection>
+            <main className="main">
+                <h1 className="saga-title">Films</h1>
+                {starWarsFilms.map((film, index) => (
+                    <FilmSection key={index} film={film} />
+                ))}
+                <section className="carousel-section-container">
+                    <h2>Spin-off</h2>
+                    <Carousel films={spinOff} />
+                </section>
+
+            </main>
             <Footer></Footer>
         </>
     )
