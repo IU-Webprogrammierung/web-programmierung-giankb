c3a6319cff37741264c5d71c512bcffb684bedd1
DESKTOP-120F7OB\Win <gianlucabattisti96@gmail.com>
2025-02-08 19:08:13 +0100

    refactor(footer): removing unused footer links

--- web-programmierung/src/ui/Footer.jsx
+++ web-programmierung/src/ui/Footer.jsx
@@ -3,22 +3,11 @@ import "./Footer.css"
 export default function Footer(){
     return (
         <footer className="footer">
-            <div className="footer-content">
-                <p>© 2025 Star Wars Fan Site. All rights reserved.</p>
-
+            <nav className="footer-content">
                 <nav className="footer-links">
-                    <a href="/privacy-policy">Privacy Policy</a>
-                    <a href="/terms">Terms & Conditions</a>
-                    <a href="/contact">Contact</a>
-                    <a href="/impressum">Impressum</a>
+                    <a href="https://www.facebook.com/StarWars.it" target="_blank" rel="noopener noreferrer">Facebook</a>
+                    <a href="https://www.instagram.com/starwars/" target="_blank" rel="noopener noreferrer">Instagram</a>
                 </nav>
-
-                <div className="social-media">
-                    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
-                    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
-                    <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
-                    <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">YouTube</a>
-                </div>
 
                 <div className="impressum">
                     <p>Impressum</p>
@@ -27,8 +16,9 @@ export default function Footer(){
                     <p>Email: contact@starwarsfansite.com</p>
                     <p>Company Registration Number: 123456789</p>
                 </div>
-            </div>
 
+                <p>© 2025 Star Wars Fan Site. All rights reserved.</p>
+            </nav>
         </footer>
     )
 }
